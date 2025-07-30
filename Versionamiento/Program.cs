2c4d4545c328fbe2c2cee7c4ff57d11b91a9fac0
Ana Laura Oliva <aolivac4@miumg.edu.gt>
2025-07-29 21:32:39 -0600

    La segunda parte

--- Versionamiento/Program.cs
+++ Versionamiento/Program.cs
@@ -1,2 +1,6 @@
 ﻿//Esta es una prueba de la clase de Sistemas Operativos donde hare 5 versiones diferentes para subir a github mi codigo
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Esta es mi segunda linea de codigo");
+
+
